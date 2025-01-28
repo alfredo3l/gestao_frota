d7612d50961d97e73530e621bda5e331aac817b1
Paulo Morales <aicrus.oficial@gmail.com>
2025-01-28 06:25:19 -0400

    fix: update tailwind and postcss configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -22,5 +20,9 @@ export default {
       },
     },
   },
-  plugins: [],
-} satisfies Config; 
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+};
+
+export default config; 
